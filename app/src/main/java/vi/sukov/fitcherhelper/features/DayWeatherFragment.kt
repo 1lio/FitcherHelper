15d59a5cd1425a50df6c35e63ebcef3ce317071a
ViacheslavSukhov <svo_efremov@mail.ru>
2020-08-11 16:48:26 +0300

    Add binding

--- app/src/main/java/vi/sukov/fitcherhelper/features/DayWeatherFragment.kt
+++ app/src/main/java/vi/sukov/fitcherhelper/features/DayWeatherFragment.kt
@@ -1,14 +1,27 @@
 package vi.sukov.fitcherhelper.features
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fr_weather_day.*
 import vi.sukov.fitcherhelper.R
 import vi.sukov.fitcherhelper.core.repository.FakeRepository
 import vi.sukov.fitcherhelper.features.weather.WeatherDay
 
-class DayWeatherFragment : Fragment(R.layout.fr_weather_day) {
+class DayWeatherFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fr_weather_day, container, false)
+    }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -16,7 +29,10 @@ class DayWeatherFragment : Fragment(R.layout.fr_weather_day) {
         val repository = FakeRepository()
         val currentDay = repository.getWeatherOfDay()
 
-        setDayInfo(currentDay)
+
+        // val binding = DataBindingUtil.inflate(LayoutInflater.from(activity), R.layout.fr_weather_day, null, false)
+
+        // setDayInfo(currentDay)
     }
 
     private fun setDayInfo(day: WeatherDay) {
