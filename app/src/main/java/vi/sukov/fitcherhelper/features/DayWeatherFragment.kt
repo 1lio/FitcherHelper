a74235edaeef8f53e104c24923cc5df2c564844c
ViacheslavSukhov <svo_efremov@mail.ru>
2020-08-11 15:24:19 +0300

    Short entry

--- app/src/main/java/vi/sukov/fitcherhelper/features/DayWeatherFragment.kt
+++ app/src/main/java/vi/sukov/fitcherhelper/features/DayWeatherFragment.kt
@@ -1,21 +1,14 @@
 package vi.sukov.fitcherhelper.features
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fr_weather_day.*
 import vi.sukov.fitcherhelper.R
 import vi.sukov.fitcherhelper.core.repository.FakeRepository
 import vi.sukov.fitcherhelper.features.weather.WeatherDay
 
-class DayWeatherFragment : Fragment() {
-
-    override fun onCreateView(inflater: LayoutInflater, group: ViewGroup?, state: Bundle?): View? {
-        super.onCreateView(inflater, group, state)
-        return inflater.inflate(R.layout.fr_weather_day, group, false)
-    }
+class DayWeatherFragment : Fragment(R.layout.fr_weather_day) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
